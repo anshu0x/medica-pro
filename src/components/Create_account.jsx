ebebef9101516c7f7b80b2ec11c68dc39bdbbf4e
anshu0x <anshusharma6327@gmail.com>
2022-09-03 22:23:18 +0530

    bugs fixed

--- src/components/Create_account.jsx
+++ src/components/Create_account.jsx
@@ -1,124 +1,167 @@
-import React,{useState} from 'react'
-import Navbar from './Navbar'
-import './create_account.css'
-import { NavLink ,useHistory} from 'react-router-dom'
-
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import "./create_account.css";
+import { NavLink, useHistory } from "react-router-dom";
 
 function Create_account() {
-  var history=useHistory();
-  const [user , setUser] = useState({
+  var history = useHistory();
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+  const [dataIsCorrect, setDataIsCorrect] = useState(false);
 
-   firstName:"",
-    lastName:"",
-    email:"",
-    password:"",
-    cpassword:""
-});
-const [errors, setErrors] = useState({
-  firstName:"",
-    lastName:"",
-    email:"",
-    password:"",
-    cpassword:""
-});
-const[dataIsCorrect, setDataIsCorrect] = useState(false);
+  let name, value;
+  const handleInputs = (e) => {
+    //  setErrors(Validation(user));
 
-let name , value;
-const handleInputs = (e) => {
-  //  setErrors(Validation(user));
-   
     name = e.target.name;
-    value = e.target.value; 
+    value = e.target.value;
 
-    setUser({ ...user , [name]:value});
-}
+    setUser({ ...user, [name]: value });
+  };
 
-  
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     // setErrors(Validation(user));
     e.preventDefault();
-  //  setErrors(Validation(user));
-   
+    //  setErrors(Validation(user));
+
     setDataIsCorrect(true);
-    const {firstName,lastName,email,password,cpassword} = user;
-    
+    const { firstName, lastName, email, password, cpassword } = user;
+
     const res = await fetch("http://localhost:3000/register", {
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body: JSON.stringify({
-
-          firstName,lastName,email,password,cpassword
-
-        })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+        cpassword,
+      }),
     });
 
     const data = await res.json();
 
-    if( res.status === 422 || !data){
-
-        window.alert("User already exist");
-        console.log("invalid registration");
-    }else if(res.status ===423){
-        window.alert("Password doesn't match ! Please re-enter it")
-    }else if(res.status === 421){
-        window.alert("You have not entered any credentials")
-    }else{
-        window.alert("Registration Successful");
-        console.log("registration successful");
-       history.push('/login');
+    if (res.status === 422 || !data) {
+      window.alert("User already exist");
+      console.log("invalid registration");
+    } else if (res.status === 423) {
+      window.alert("Password doesn't match ! Please re-enter it");
+    } else if (res.status === 421) {
+      window.alert("You have not entered any credentials");
+    } else {
+      window.alert("Registration Successful");
+      console.log("registration successful");
+      history.push("/login");
     }
-
-} 
+  };
 
   return (
-    <div>
+    <>
+      <Navbar />
+      <div className="forms_login">
+        <form action="">
+          <div class="signupform-container">
+            <h1>Register</h1>
+            <p>Please fill in this form to create an account.</p>
 
+            <label for="fname">
+              <b>First Name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="first name"
+              name="firstName"
+              value={user.firstName}
+              id="fname"
+              required
+              onChange={handleInputs}
+            />
+            <label for="fname">
+              <b>Last Name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="last name"
+              name="lastName"
+              value={user.lastName}
+              id="lname"
+              required
+              onChange={handleInputs}
+            />
 
-<Navbar/>
+            <label for="email">
+              <b>Email</b>
+            </label>
+            <input
+              type="text"
+              placeholder="medicee@mail.com"
+              name="email"
+              value={user.email}
+              id="email"
+              required
+              onChange={handleInputs}
+            />
 
-<div>
+            <label for="psw">
+              <b>Password</b>
+            </label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              name="password"
+              value={user.password}
+              id="psw"
+              required
+              onChange={handleInputs}
+            />
 
-<div className='forms_login'>
+            <label for="psw-repeat">
+              <b>Repeat Password</b>
+            </label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              name="cpassword"
+              value={user.cpassword}
+              id="psw-repeat"
+              required
+              onChange={handleInputs}
+            />
 
+            <hr />
+            <p>
+              By creating an account you agree to our{" "}
+              <a href="#">Terms & Privacy</a>.
+            </p>
 
-<form action="">
-  <div class="container">
-    <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
+            <button type="submit" onClick={handleSubmit} class="registerbtn">
+              Register
+            </button>
+          </div>
 
-    <label for="fname"><b>First Name</b></label>
-    <input type="text" placeholder="first name" name="firstName" value={user.firstName} id="fname"  required onChange={handleInputs}  />
-    <label for="fname"><b>Last Name</b></label>
-    <input type="text" placeholder="last name" name="lastName" value={user.lastName} id="lname"  required onChange={handleInputs}  />
-
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="medicee@mail.com" name="email" value={user.email} id="email"  required onChange={handleInputs}  />
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="••••••••" name="password" value={user.password} id="psw"  required onChange={handleInputs}  />
-
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="••••••••" name="cpassword" value={user.cpassword} id="psw-repeat"  required onChange={handleInputs}  />
-
-    
-    <hr/>
-    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-
-    <button type="submit" onClick={handleSubmit} class="registerbtn">Register</button>
-  </div>
-  
-  <div class="container signin">
-    <p>Already have an account? <NavLink to="/login">Sign in </NavLink>.</p>
-  </div>
-</form>
-
-</div>
-
-</div>
-    </div>
-  )
+          <div class="signupform-container signin">
+            <p>
+              Already have an account? <NavLink to="/login">Sign in </NavLink>.
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Create_account
+export default Create_account;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,106 +1,108 @@
-import React,{useState} from 'react'
-import './Login.css'
-import { NavLink,useHistory } from 'react-router-dom'
-import Navbar from './Navbar'
-
+import React, { useState } from "react";
+import "./Login.css";
+import { NavLink, useHistory } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function Login() {
-  var kode=true;
-  var history=useHistory();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  var kode = true;
+  var history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const loginPage = async (e) =>{
+  const loginPage = async (e) => {
+    e.preventDefault();
 
-       e.preventDefault();
-    
-       if(email=="medicee@org.com" && password=="Medicee@1"){
-        history.push('/admin_dashboard');
-        window.alert('Logged in As Admin');
-        kode=false;
-      }
-    if(kode==true){
+    if (email === "medicee@org.com" && password === "Medicee@1") {
+      history.push("/admin_dashboard");
+      window.alert("Logged in As Admin");
+      kode = false;
+    }
+    if (kode === true) {
       const res = await fetch("http://localhost:3000/signin", {
-          method:"POST",
-          headers:{
-              "Content-Type":"application/json"
-          },
-          body: JSON.stringify({
-
-              email,password
-
-          })
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
       });
-      
 
       const data = res.json().then((user) => {
         return user.userLoginMail.email;
       });
-      
+
       const printuser = async () => {
-         const a = await data;
-         sessionStorage.setItem('emailid',a);
+        const a = await data;
+        sessionStorage.setItem("emailid", a);
       };
-    
+
       printuser();
 
-      const eml=sessionStorage.getItem('emailid');
+      const eml = sessionStorage.getItem("emailid");
       console.log(eml);
-     
 
-      if( res.status === 400 || !data){ 
-
-          window.alert("Invalid Registartion");
-          console.log("invalid registration");
-      }else{
-          history.push('/dashboard');
-          //window.alert("Logged in Successful");
-           
+      if (res.status === 400 || !data) {
+        window.alert("Invalid Registartion");
+        console.log("invalid registration");
+      } else {
+        history.push("/dashboard");
+        //window.alert("Logged in Successful");
       }
-
     }
-  }
+  };
 
   return (
-    <div>
+    <>
+      <Navbar />
+      <div>
+        <div className="forms_login">
+          <form action="">
+            <div className="formcontainer">
+              <h1>Log In</h1>
+              <label htmlFor="email">
+                <b>Email</b>
+              </label>
+              `
+              <input
+                type="text"
+                placeholder="medicee@mail.com"
+                name="email"
+                id="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="psw">
+                <b>Password</b>
+              </label>
+              <input
+                type="password"
+                placeholder="••••••••"
+                name="password"
+                id="psw"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <hr />
+              <button type="submit" className="registerbtn" onClick={loginPage}>
+                Sign in
+              </button>
+            </div>
 
-<Navbar/>
-
-<div>
-
-<div className='forms_login'>
-
-
-<form action="">
-  <div className="container">
-    <h1>Log In</h1>
-
-    
-    <label htmlFor="email"><b>Email</b></label>`
-    <input type="text" placeholder="medicee@mail.com" name="email" id="email" required  value={email} onChange={(e) =>setEmail(e.target.value)} />
-
-    <label htmlFor="psw"><b>Password</b></label>
-    <input type="password" placeholder="••••••••" name="password" id="psw" required value={password} onChange={(e) =>setPassword(e.target.value)} />
-
-    
-    <hr/>
-
-    <button type="submit" className="registerbtn" onClick={loginPage} >Sign in</button>
-  </div>
-  
-  <div className="container signin">
-    <p>Create an account? <NavLink to="/create_account">Sign Up</NavLink>.</p>
-  </div>
-</form>
-
-</div>
-
-</div>
-
-
-
-    </div>
-  )
+            <div className="formcontainer signin">
+              <p>
+                Create an account?{" "}
+                <NavLink to="/create_account">Sign Up</NavLink>.
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
